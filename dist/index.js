361f58694315fffb01f1ba4020efccbcca9a95a7
Elin Sopiah <gitgit367@gmail.com>
2023-03-13 15:46:51 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,24 @@
 const person = {
     name: 'Elma',
     age: 20,
-    gander: 'Female'
+    gander: 'Female',
+    books: [
+        {
+            title: 'Jago Ngoding Semalem',
+            author: 'Diki Yusup'
+        }
+    ]
 };
 person.color = 'black';
+person.isDead = true;
+person.great = (name) => `Hawwo ${name}`;
+console.log(person.great('Momon'));
 console.log(person);
+class Human {
+    constructor(name, age) {
+        this.name = name,
+            this.age = age;
+    }
+}
+const momo = new Human('elmon nur', 20);
+console.log(momo);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,53 @@
-type person = {
+interface person {
     name : string,
     age : number,
     gander : string,
     color? : string,
     isDead? : boolean,
+    y? : any,
+    great? : ( name: string) => string,
+    parent ?:{
+        name : string,
+        age : number,
+        gander : string,
+    },
+    books?: {
+        title : string,
+        author : string,
+    }[],
 }
 
 const person: person ={
     name : 'Elma',
     age : 20,
-    gander : 'Female'
+    gander : 'Female',
+    books : [
+        {
+            title: 'Jago Ngoding Semalem',
+            author: 'Diki Yusup'
+        }
+    ]
 }
 
 person.color = 'black'
 person.isDead = true
+
+person.great = (name) => `Hawwo ${name}`;
+
+console.log(person.great('Momon'));
+
+
 console.log(person );
+
+class Human {
+    public name : string
+    public age : number
+
+    constructor(name: string, age: number ) {
+        this.name = name,
+        this.age = age
+    }
+}
+
+const momo = new Human('elmon nur', 20)
+console.log(momo);
